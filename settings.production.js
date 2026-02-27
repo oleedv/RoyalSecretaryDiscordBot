bc47b84732e59b260993e08d8c0a47e055e1aa38
OleEd <olenorholm55@gmail.com>
2026-02-27 16:28:31 +0100

    chore: populate production settings with Discord IDs

--- settings.production.js
+++ settings.production.js
@@ -9,33 +9,37 @@ export default {
   },
   webBaseUrl: 'https://royalbattalion.xyz',
   guild: {
-    id: '',                           // Production guild ID
+    id: '458272992892420108',                           // Production guild ID
   },
   channels: {},
   roles: {},
   tickets: {
-    categoryId: '',                   // Ticket category
-    panelChannelId: '',               // "Create Ticket" panel channel
+    categoryId: '1476956330496364644',                   // Ticket category
+    panelChannelId: '1476956038975455423',               // "Create Ticket" panel channel
     roles: {
-      normal: [''],                   // RB member role
-      communityOfficer: [''],         // CO role
-      adminOfficer: [''],             // Admin role
-      compTeam: [''],                 // Comp Team role
-      whitelist: [''],                // Whitelist role
+      normal: ['458273494048964619', '458277129973661719', '733974178797191189'],                   // RB member role, Server admin, RB admin, RB managers
+      communityOfficer: ['989894740642263082'],         // CO role
+      adminOfficer: ['810252560220946432'],             // Admin role
+      compTeam: ['1291154622656020570', '1048696124233502760'],                 // Comp Team role, comp leader, comp manager
+      whitelist: ['917911950120333323'],                // Whitelist role
     },
   },
   prospects: {
-    categoryId: '',                   // Prospect category
-    panelChannelId: '',               // "Join RB" panel channel
-    forumChannelId: '',               // Forum channel for public posts
-    loungeChannelId: '',              // Public announcements (voting, accepted)
+    categoryId: '1476956405977059490',                   // Prospect category
+    panelChannelId: '1476956038975455423',               // "Join RB" panel channel
+    forumChannelId: '1476956459211423814',               // Forum channel for public prospect posts
+    loungeChannelId: '1476955931794473141',              // Public announcements (voting, accepted)
     periodDays: 21,
     voteDaysBefore: 7,
-    roles: [''],                      // Staff roles
+    roles: ['1204763136830218260', '654022866261770270', '1213958671843860552', '1295470787934814259', '989894740642263082', '733974178797191189'],                      // Staff roles, trainee mentor, mentor, seniro mentor, recruitment officer, comm officer, rb managers
     prospectRoleId: '',               // Prospect role
     whitelistRoleId: '',              // Whitelist role
     mentorRoleId: '',                 // Mentor role
-    voiceChannelId: '',               // Voice channel for invites
+    voiceChannelId: '1476956237110186195',               // Voice channel for invites
+    memberHubChannelId: '1051879922643255306',            // Member hub channel
+    meetTheMembersChannelId: '458279712213565440',       // Meet the members channel
+    goingAwayChannelId: '1237155781531402270',            // Going away channel
+    feedbackChannelId: '1237151891214041149',             // Prospect feedback channel
     voteEmojis: {
       yes: '',                        // YES emoji ID
       no: '',                         // NO emoji ID
@@ -43,7 +47,7 @@ export default {
     },
   },
   dm: {
-    infoChannelId: '',                  // Server info/rules channel
+    infoChannelId: '1164909963823558778',                  // Server info/rules channel
   },
   battlemetrics: {
     organisationId: '13904',
@@ -58,7 +62,7 @@ export default {
     schedulerCheckMs: 60000,
   },
   serverStatus: {
-    channelId: '',                    // Server status channel
+    channelId: '1476956161566572705',                    // Server status channel
     updateIntervalMs: 60000,
   },
 };
